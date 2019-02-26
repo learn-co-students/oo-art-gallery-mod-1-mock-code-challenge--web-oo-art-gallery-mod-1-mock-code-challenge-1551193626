09fe9126def1b0a5edc226686db65d004d1796f7
github username <github email address>
2019-02-26 11:40:52 -0500

    done

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,22 @@
+#MANY 
+
 class Gallery
 
-  attr_reader :name, :city
+	@@all = []
+
+  attr_reader :name, :city, :paintings
 
   def initialize(name, city)
     @name = name
     @city = city
+    @paintings = []
+    @@all << self
   end
+
+ def self.all 
+ 	@@all 
+ end
+
+
 
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,25 @@
+#JOIN
+
 class Painting
 
-  attr_reader :title, :price
+  attr_reader :title, :price, :artist
 
   @@all = []
 
   def initialize(title, price)
     @title = title
     @price = price
+    @artist = artist
+    @@all << self
   end
+
+  def self.all 
+  	@@all 
+  end
+
+  # def self.total_price
+  # 	Painting.all.select |painting| 
+  # 		self.price 
+  # end
 
 end
