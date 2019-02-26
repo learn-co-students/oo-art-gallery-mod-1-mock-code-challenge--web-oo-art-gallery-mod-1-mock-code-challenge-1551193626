dfb738070e123159e70de8f3549a66ffba5e52d0
sadiebennett1 <sadiebenn1@gmail.com>
2019-02-26 11:42:11 -0500

    send

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,53 @@
 class Artist
 
   attr_reader :name, :years_experience
+  @@all = []
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
 
+  def paintings
+    Painting.all.select do |painting|
+      painting.artist == self.name
+  end
+
+  def galleries
+    self.paintings.map do |painting|
+      painting.gallery
+  end
+
+  def cities
+    final_arr = []
+    Gallery.all.each do |gallery|
+      if self.galleries.include?(gallery.name)
+        final_arr << gallery.city
+      end
+    end
+    return final_arr
+  end
+
+  def total_experience
+    sum = 0
+    @@all.each do |arist_arr|
+      artist_arr.each do |name, years|
+        sum += years
+    end
+    return sum
+  end
+
+  def most_prolific
+    nums = {}
+    @@all.each do |self|
+      nums[self.name] = self.paintings.length / self.years_experience
+    end
+    @@all.select do |
+
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
+  end
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,24 @@
 class Painting
 
-  attr_reader :title, :price
+  attr_reader :title, :price, :artist, :gallery
 
   @@all = []
 
-  def initialize(title, price)
+  def initialize(title, price, artist, gallery)
     @title = title
     @price = price
+    @artist = artist
+    @gallery = gallery
+    @@all << self
+  end
+
+  def total_price
+    sum = 0
+    @@all.each do |painting|
+      painting.each do |title, price, artist, gallery|
+        sum += price
+    end
+    return sum
   end
 
 end
