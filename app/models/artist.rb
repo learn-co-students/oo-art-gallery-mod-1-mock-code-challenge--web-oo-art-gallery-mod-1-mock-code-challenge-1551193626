905d7be907b4c0366827eb922ae20c2942c486f9
ashhcashh1120 <aarchibald20@gmail.com>
2019-02-26 11:41:36 -0500

    I NEED TO STUDY

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,33 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
+
+
+
+      #Returns an `array` of .all the artists
+      artist.name = name
+
+      # Returns an `array` all the #paintings by an artist
+
+
+      # Returns an `array` of all the #galleries that an artist has paintings in
+
+      #Return an `array` of all #cities that an artist has paintings in
+
+
+    # Returns an `integer` that is the .total_experience of all artists
+
+      #`Artist.most_prolific`
+      # Returns an `instance` of the artist with the highest amount of paintings per year of experience.
+
+    #`Artist#create_painting`
+      # Given the arguments of `title`, `price` and `gallery`, creates a new painting belonging to that artist
+
 
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -7,6 +7,19 @@ class Painting
   def initialize(title, price)
     @title = title
     @price = price
+    @@all << self
+
+  end
+
+  #returns array of all the paintings
+  def self.all
+      @title << @@all
+  end
+
+  #returns and integer that is the total price
+  #of all paintings
+  def self.total_price
+    @price << @@all
   end
 
 end
