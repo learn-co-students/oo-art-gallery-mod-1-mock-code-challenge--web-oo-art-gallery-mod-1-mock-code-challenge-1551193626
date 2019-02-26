058e4e752c46bf1248d1a7916c11962144e8359e
cstruong <christophertruongm@gmail.com>
2019-02-26 11:41:18 -0500

    wtf did i just do?

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,61 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all.push(self)
+  end
+
+  def paintings
+    Painting.all.select {|painting|
+      paiting.artist == self
+    }
+  end
+
+  def galleries
+    paintings.map {|artists_painting|
+      artists_painting.gallery
+    }
+  end
+
+  def cities
+    galleries.map {|artists_paintings_gallery|
+      artists_paintings_gallery.city
+    }
+  end
+
+  def self.total_experience
+    total = nil
+    self.all.each {|artist|
+      total += artist.years_experience
+    }
+    return total
+  end
+
+  def self.most_prolific
+    most_prolific_artist = nil
+    highest_years = 0
+
+    self.all.each {|artist|
+      if artist.years_experience > highest_years
+        highest_years = artist.years_experience
+        most_prolific_artist = artist.name
+      end
+    }
+    return most_prolific_artist
+  end
+
+  def create_painting(title, price, gallery)
+    newPainting = Painting.new(title, price, gallery)
+    newPainting.artist = self
+    newPainting
+  end
+
+  def self.all
+    @@all
   end
 
 end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,9 +2,48 @@ class Gallery
 
   attr_reader :name, :city
 
+  @@all = []
+
   def initialize(name, city)
     @name = name
     @city = city
+    @@all.push(self)
+  end
+
+  def paintings
+    Painting.all.select {|painting|
+      painting.gallery == self.name
+    }
+  end
+
+  def artists
+    paintings.map {|galleries_painting|
+      galleries_painting.artist
+    }
+  end
+
+  def artist_names
+    artists.map {|galleries_paintings_artist|
+      galleries_paintings_artist.name
+    }
+  end
+
+  def most_expensive_painting
+    highest_price = 0
+    most_expensive = ''
+
+    paintings.each {|painting|
+      if painting.price > highest_price
+        highest_price = painting.price
+        most_expensive = painting.self
+      end
+    }
+
+    return most_expensive
+  end
+
+  def self.all
+    @@all
   end
 
 end
