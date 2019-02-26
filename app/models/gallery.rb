c7fd227d5f674363ace79b47f779e9635c6e89c0
Juan Alorro <juan.alorro@flatironschool.com>
2019-02-26 11:42:02 -0500

    Done

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,43 @@
+require 'pry'
+
 class Gallery
+
+  @@all = []
 
   attr_reader :name, :city
 
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select {|painting| painting.gallery == self}
+  end
+
+  def artists
+    paintings.map {|painting| painting.name}
+  end
+
+  def artist_names
+    paintings.map {|painting| painting.gallery}
+  end
+
+  def most_expensive_painting
+    checker = nil
+
+    Painting.all.select do |painting|
+      painting.find do |title, price|
+        if checker == nil || price > checker
+          checker = price
+        end
+      end
+    end
   end
 
 end
