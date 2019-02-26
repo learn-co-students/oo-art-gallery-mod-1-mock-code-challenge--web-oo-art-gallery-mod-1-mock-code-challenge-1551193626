a267a969854abad631b8b4b4be60f0de4e7e4334
Simon Mei <smei8800@gmail.com>
2019-02-26 11:40:49 -0500

    Donezo

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,9 +2,48 @@ class Gallery
 
   attr_reader :name, :city
 
+  @@all = []
+
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select do |painting|
+      painting.gallery == self
+    end
+  end
+
+  def artists
+    artist_list = paintings.collect do |painting|
+      painting.artist
+    end
+    artist_list.uniq
+  end
+
+  def artist_names
+    artist_names = artists.collect do |artist|
+      artist.name
+    end
+    artist_names.uniq
+  end
+
+  def most_expensive_painting
+    pricey_painting = nil
+    price = 0
+    paintings.each do |painting|
+      if painting.price > price
+        price = painting.price
+        pricey_painting = painting
+      end
+    end
+    pricey_painting
   end
 
 end
