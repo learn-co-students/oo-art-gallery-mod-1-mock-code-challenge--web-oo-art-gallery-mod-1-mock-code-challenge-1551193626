39cab22132f3a82d4af567404fda40f3102d5b6f
James Dorr <jamesdorr3@gmail.com>
2019-02-26 11:41:37 -0500

    I want to fix that .most_prolific so badly. The rest went well.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,9 +2,36 @@ class Gallery
 
   attr_reader :name, :city
 
+  @@all = []
+
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
+  end
+
+  def display_painting(paintingname)
+    paintingname.gallery = self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select {|painting_inst| painting_inst.gallery = self}
+  end
+
+  def artists
+    self.paintings.map {|painting| painting.artist}
+  end
+
+  def artist_names
+    self.artists.map {|artist| artist.name}
+  end
+
+  def most_expensive_painting
+    Painting.all.max {|a, b| a.price <=> b.price}
   end
 
 end
