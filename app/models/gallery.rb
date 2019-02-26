71292478355fb3262c3c6b06abbbb03003739c0f
merestrickland <mere.strickland@gmail.com>
2019-02-26 11:41:38 -0500

    sos

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,9 +2,26 @@ class Gallery
 
   attr_reader :name, :city
 
+  @@all = []
+
   def initialize(name, city)
     @name = name
     @city = city
+  end
+
+  def paintings
+    #returns an array of all paintings in the gallery...
+  end
+
+  def artists
+    #Returns an array of all artists that have a painting in a gallery
+  end
+
+  def artist_names
+  end
+
+  def most_expensive_painting
+    #
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,11 @@
 require_relative '../config/environment.rb'
 
 
+meredith = Artist.new('mere', 10)
 
+new_painting = Painting.new('sunset', 1000)
+
+meredith.create_painting('dogs', 900, 'LES')
 
 binding.pry
 
